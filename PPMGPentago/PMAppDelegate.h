0c2173d5c76664bc04a9a995ba68d4cc2b8ed8e3
student <none@none.com>
2014-09-10 10:09:51 -0700

    Initial Commit

---
+++ PPMGPentago/PMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PMAppDelegate.h
+//  PPMGPentago
+//
+//  Created by student on 9/10/14.
+//  Copyright (c) 2014 PPMGLLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PPMGPentago/PMViewController.m
@@ -0,0 +1,29 @@
+//
+//  PMViewController.m
+//  PPMGPentago
+//
+//  Created by student on 9/10/14.
+//  Copyright (c) 2014 PPMGLLC. All rights reserved.
+//
+
+#import "PMViewController.h"
+
+@interface PMViewController ()
+
+@end
+
+@implementation PMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
